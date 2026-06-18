887f7ae47318e9ef058151678d83ab1947caf099
Nathã Melo <nathamelo2007@gmail.com>
2026-06-18 19:03:28 -0300

    feat: initial commit

---
+++ src/main/java/com/api/manager_projects/ManagerProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.api.manager_projects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/manager_projects/ManagerProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.manager_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagerProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
